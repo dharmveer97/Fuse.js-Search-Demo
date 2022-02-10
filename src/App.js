8e8291d15a016095a71128f1f83100801a296da3
dharmveer97 <dvbangar8@gmail.com>
2022-02-10 20:26:23 +0530

    search bar test

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Fuse from 'fuse.js';
 import characters from './characters.json';
 
 function App() {
+
   const [query, updateQuery] = useState('');
 
   const fuse = new Fuse(characters, {
     keys: [
       'name',
       'company',
-      'species'
     ],
     includeScore: true
   });
@@ -25,22 +25,15 @@ function App() {
 
   return (
     <>
-
-      <header className="App-header">
-        <div className="container">
-          <h1>Futurama Characters</h1>
-        </div>
-      </header>
-
       <main className="container">
+
         <ul className="characters">
+        <input className="search" placeholder="Search..." type="text" value={query} onChange={onSearch} />
+
           {characterResults.map(character => {
-            const { name, company, species, thumb } = character;
+            const { name, company, thumb } = character;
             return (
               <li key={name} className="character">
-                <span className="character-thumb" style={{
-                  backgroundImage: `url(${thumb})`
-                }} />
                 <ul className="character-meta">
                   <li>
                     <strong>Name:</strong> { name }
@@ -48,29 +41,12 @@ function App() {
                   <li>
                     <strong>Company:</strong> { company }
                   </li>
-                  <li>
-                    <strong>Species:</strong> { species }
-                  </li>
                 </ul>
               </li>
             )
           })}
         </ul>
-        <aside>
-          <form className="search">
-            <label>Search</label>
-            <input type="text" value={query} onChange={onSearch} />
-          </form>
-        </aside>
       </main>
-
-      <footer>
-        <div className="container">
-          <p>
-            Images from <a href="http://www.cc.com/shows/futurama">Futurama</a> via <a href="https://futurama.fandom.com/">futurama.fandom.com</a>
-          </p>
-        </div>
-      </footer>
     </>
   );
 }
